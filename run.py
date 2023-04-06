5069aaba07e6b3ec70eb550e4219fa1ed5b63332
Baz <111312411+Barryk112@users.noreply.github.com>
2023-04-06 19:22:40 +0000

    Added member.html, when a member is clicked on the about page it will open a new page dedicated to the memeber, logig was added to run.py to accomadate this

--- run.py
+++ run.py
@@ -31,6 +31,19 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+# Runs when ou cick on a member
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    memeber = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        # loops and gets correct member object
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
